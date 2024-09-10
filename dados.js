22505be917cbd49d2433b8b7bf36e5186fd13506
phdias <pedropd.barbosa@gmail.com>
2024-09-10 18:27:34 -0300

    create .github folder

---
+++ dados.js
@@ -0,0 +1,46 @@
+let dados = [
+  {
+    titulo: "Hunter 350: Estilo e desempenho em uma moto retrô",
+    descricao: "A Royal Enfield Hunter 350 é uma motocicleta que une o charme das clássicas com a praticidade do dia a dia. Com design atemporal e motor confiável, ela oferece uma experiência de pilotagem suave e prazerosa, sendo uma ótima opção para quem busca uma moto versátil e cheia de estilo.",
+    marca: "Royal Enfield",
+    link: "https://ofertasroyalenfield.com.br/hunter-350",
+    tags: "hunter 350 hunter royalenfield",
+    thumbnail: {
+      image: "thumbnails/HUNTER350.png",
+      alt: "Hunter 350 cor vermelha"
+    }
+  },
+  {
+    titulo: "FZ15 ABS: A esportiva para o dia a dia",
+    descricao: "A Yamaha FZ15 ABS é uma motocicleta esportiva com design moderno e motor potente.Ideal para quem busca agilidade e desempenho em um modelo versátil, a FZ15 ABS é perfeita para o uso urbano e pequenas viagens.Seu sistema de freios ABS garante maior segurança nas frenagens.",
+    marca: "Yamaha",
+    link: "https://www.yamaha-motor.com.br/fazer-fz15-abs/product/30094",
+    tags: "fz15 fz15abs 150 abs",
+    thumbnail: {
+      image: "thumbnails/FZ15ABS.png",
+      alt: "FZ15 ABS cor vermelha"
+    }
+  },
+  {
+    titulo: "CG Titan 160: A clássica que nunca sai de moda",
+    descricao: "A Honda CG Titan 160 é um ícone do motociclismo nacional, conhecida por sua robustez e economia. Com design atemporal e motor eficiente, ela é a escolha perfeita para quem busca uma moto confiável e durável para o dia a dia.",
+    marca: "Honda",
+    link: "https://www.honda.com.br/motos/street/city/cg-160-titan",
+    tags: "160 cg cgtitan titan",
+    thumbnail: {
+      image: "thumbnails/CGTITAN160.png",
+      alt: "CG TITAN 160 cor vermelha"
+    }
+  },
+  {
+    titulo: "MT03: A Naked que define a nova geração",
+    descricao: "A MT03 da Yamaha é a escolha ideal para quem busca uma moto naked com design agressivo e desempenho de alto nível. Com um motor de 321cc, a MT03 oferece uma combinação perfeita de potência e agilidade, tornando-a ideal tanto para o uso urbano quanto para estradas.",
+    marca: "Yamaha",
+    link: "https://www.yamaha-motor.com.br/mt-03-abs/product/30121",
+    tags: "mt03 yamaha naked motocicleta esportiva",
+    thumbnail: {
+      image: "thumbnails/MT03.png",
+      alt: "Yamaha MT03 em cor azul"
+    }
+  }  
+]
